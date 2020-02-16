4f1ab38ff16661b0d478df9ce3a6564ff435d1ef
Shajedul Hoque <shajed@callnsolution.com>
2020-02-16 22:45:54 +0600

    server running issue fix

--- admin/entrypoint.sh
+++ admin/entrypoint.sh
@@ -3,4 +3,4 @@
 RAILS_ENV=development rails db:create
 RAILS_ENV=development rails db:migrate
 RAILS_ENV=development rails db:seed
-RAILS_ENV=development bundle exec puma -p 3000 -C config/puma.rb -D
+RAILS_ENV=development bundle exec puma -p 3000 -C config/puma.rb
