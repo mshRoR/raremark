e04ef467d70f78fad249bcfb876c276a99307606
Shajedul Hoque <shajed@callnsolution.com>
2020-02-20 00:17:45 +0600

    search graph chart added and render

--- search/app/helpers/searches_helper.rb
+++ search/app/helpers/searches_helper.rb
@@ -1,5 +1,13 @@
 module SearchesHelper
-    def profile_chart(ids)
+    def gender_chart(ids)
         Profile.where(user_id: ids).group(:gender).count
+    end
+
+    def country_chart(ids)
+        Profile.joins(:country).where(user_id: ids).group('countries.name').count
+    end
+
+    def profile_type_chart(ids)
+        DiseaseHistory.joins(:disease).where(user_id: ids).group('diseases.name').count
     end
 end
